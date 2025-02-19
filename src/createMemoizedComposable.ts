4d7c05629356e7d3b519b12f1345dfc94ca435ab
croatialu <wuli.croatia@foxmail.com>
2025-02-19 08:37:17 +0000

    fix: toggle

--- src/createMemoizedComposable.ts
+++ src/createMemoizedComposable.ts
@@ -2,18 +2,11 @@
  * Thanks to vueuse
  * @link https://github.com/vueuse/vueuse/blob/main/packages/shared/createSharedComposable/index.ts
  */
-import type { EffectScope } from 'vue'
 import { useQueryClient } from '@tanstack/vue-query'
-import { effectScope, markRaw } from 'vue'
+import { isRef, watch } from 'vue'
 import { tryOnScopeDispose } from './tryOnScopeDispose'
 
 let counter = 0
-
-interface State {
-  value: any
-  subscribers: number
-  scope: EffectScope
-}
 
 /**
  * Memoize a composable
@@ -22,57 +15,29 @@ interface State {
  */
 export function createMemoizedComposable<T extends (...args: any[]) => any>(composable: T): T {
   counter++
+  const createQueryKey = (...args: any[]): any[] => ['CREATE_MEMOIZED_COMPOSABLE', counter, ...args]
 
   return <T>((...args) => {
     const queryClient = useQueryClient()
 
-    const key = ['CREATE_STD_SHARE_COMPOSABLE', counter, ...args]
+    const key = createQueryKey(...args)
 
-    const getState = (): State | undefined => queryClient.getQueryData<State>(key)
-    let state = getState()
+    let state = queryClient.getQueryData(key)
 
     if (!state) {
-      const scope = effectScope(true)
-      const value = scope.run(() => composable(...args))
-
-      const newState = {
-        value,
-        subscribers: 1,
-        scope: markRaw(scope),
-      }
-
-      queryClient.setQueryData(key, newState)
+      const newState = composable(...args)
       state = newState
-    }
-    else {
-      const newSubscribers = state.subscribers + 1
+      queryClient.setQueryData(key, state)
 
-      queryClient.setQueryData(key, {
-        ...state,
-        subscribers: newSubscribers,
+      watch(key.filter(v => isRef(v)), () => {
+        queryClient.setQueryData(key, newState)
+      }, { deep: true })
+
+      tryOnScopeDispose(() => {
+        queryClient.removeQueries({ queryKey: key })
       })
     }
 
-    tryOnScopeDispose(() => {
-      const state = getState()
-
-      if (!state)
-        return
-
-      const newSubscribers = state.subscribers - 1
-
-      if (newSubscribers > 0) {
-        queryClient.setQueryData(key, {
-          ...state,
-          subscribers: newSubscribers,
-        })
-        return
-      }
-
-      state.scope.stop()
-      queryClient.removeQueries({ queryKey: key })
-    })
-
-    return state.value
+    return state
   })
 }
